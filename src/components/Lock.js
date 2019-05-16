699b3481a7eb288d0d6d22539f41d06b955b71e1
Lauren Davidson <laurendavidson@Laurens-MacBook-Pro.local>
2019-05-16 14:35:48 -0400

    useEffect working

--- src/components/Lock.js
+++ src/components/Lock.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect } from 'react'
 import Button from './Button.js'
 import './lock.css'
 
@@ -19,7 +19,6 @@ function Lock() {
   )
   const [unlocked, itsOpen] = useState(false)
   const [entries, addEntry] = useState([])
-  const entriesRef = useRef()
 
   const buttons = [
       {id: 1, color: '#512DA8',},
@@ -35,33 +34,55 @@ function Lock() {
 
     useEffect(() => {
       let code = [1, 2]
-      console.log('here');
       console.log(JSON.stringify(code))
-      console.log(JSON.stringify(entries))
+      console.log(JSON.stringify(entries));
       if (JSON.stringify(code) === JSON.stringify(entries)) {
         itsOpen(true)
-        console.log('hey hey')
       }
-      })
+    }, [entries])
 
-  return (
-    <div id='pagegrid'>
-    <div id='locktitle'>
-      <h2>Enter the code...</h2>
-    </div>
-      <div id='lockcontainer'>
-        <div id='lockbox'>
-          {buttons.map(button => {return (<Button key={button.id} color={button.color} onClick={() => {
-            onPress((pressed) => ({...pressed, [button.id]: true}))
-            console.log(pressed);
-            addEntry([...entries, button.id])
-            console.log(entries);
-          }} pressed={pressed[button.id]} />)})}
+  if (!unlocked) {
+    return (
+      <div id='pagegrid'>
+      <div id='locktitle'>
+        <h2>Enter the code...</h2>
+      </div>
+        <div id='lockcontainer'>
+          <div id='lockbox'>
+            {buttons.map(button => {return (<Button key={button.id} color={button.color} onClick={() => {
+              onPress((pressed) => ({...pressed, [button.id]: true}))
+              console.log(pressed);
+              addEntry([...entries, button.id])
+              console.log(entries);
+            }} pressed={pressed[button.id]} />)})}
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
 
+  }
+  else if (unlocked) {
+    return (
+      <div id='pagegrid'>
+      <div id='locktitle'>
+        <h2>Success!</h2>
+      </div>
+        <div id='lockcontainer'>
+          <div id='lockbox'>
+            {buttons.map(button => {return (<Button key={button.id} color={button.color} onClick={() => {
+              onPress((pressed) => ({...pressed, [button.id]: true}))
+              console.log(pressed);
+              addEntry([...entries, button.id])
+              console.log(entries);
+            }} pressed={pressed[button.id]} />)})}
+          </div>
+        </div>
+          <div id='success'>
+            <iframe src="https://giphy.com/embed/4xpB3eE00FfBm" width="240" height="229" frameBorder="0" allowFullScreen title='baby success'></iframe>
+        </div>
+      </div>
+    )
+  }
 
 }
 
